10bf422eceec29b39eabaf86ea2bfdfc45f8310e
Naxilde <aster9812@gmail.com>
2017-05-22 20:56:05 +0200

    Update LOL_API.py

--- LOL_API.py
+++ LOL_API.py
@@ -71,12 +71,12 @@ def obj():
 @post('/Object')
 def event():
   name3 = request.forms.get('ID')
-  payload3 = {"locale":request.forms.get('Language'),'api_key':mykey,'itemListData':'stats'}
-  r3=requests.get('https://euw1.api.riotgames.com/lol/static-data/v3/items',params=payload3)
+  payload3 = {'itemListData':'stats','locale':request.forms.get('Language'),'api_key':mykey}
+  r3=requests.get('https://euw1.api.riotgames.com/lol/static-data/v3/items?itemListData=stats&locale=es_ES',params=payload3)
   doc3 = r3.json()
   for o in doc3:
-	if doc3['data'][o]['name'] == name3.title():
-		ID3 = str(doc3['data'][o])
+	if doc3["data"][o]["name"] == name3.title():
+		ID3 = str(doc3["data"][o])
 
   payload4={"locale":request.forms.get('Language'),'api_key':mykey,'itemData':'all'}
   URL4 = 'https://euw1.api.riotgames.com/lol/static-data/v3/items/'+ID3
