e454178c4819df03b0e9645183cecffc0c717be0
Naxilde <aster9812@gmail.com>
2017-05-22 22:14:59 +0200

    Update LOL_API.py

--- LOL_API.py
+++ LOL_API.py
@@ -75,7 +75,7 @@ def event():
   r3=requests.get('https://euw1.api.riotgames.com/lol/static-data/v3/items',params=payload3)
   doc3 = r3.json()
   for o in doc3:
-	if not doc3['basic']['id'] == 0 and doc3['data'][o]['name'] == name3.title():
+	if doc3['data'][o]['name'] == name3.title():
           ID3 = str(doc3['data'][o])
 
   payload4={"locale":request.forms.get('Language'),'api_key':mykey,'itemData':'all'}
